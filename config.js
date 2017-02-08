6ba318a553d2139b573772e9b8112389991fcd07
Icyse <icyse@sina.com>
2017-02-08 23:14:34 +0800

    pretty

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	/*mongodb://user:pwd@host:port/database*/
-  	"DbPath": "mongodb://mean-blog:cccccc@localhost:27017/mean-blog",
+    /*mongodb://user:pwd@host:port/database*/
+    "DbPath": "mongodb://mean-blog:cccccc@localhost:27017/mean-blog",
 
-  	"RedisHost": "127.0.0.1",
-  	"RedisPort": 6379,
-  	"RedisPass": "cccccc",
-  	"CacheExpired": 86400,
-  	"cookieSecret" : "Game.js",
-  	"sessionExpiration": 86400,
-  	"salt": "OdwqENkHnCse8JNt3QCCFlrnvRLxZXUP"
+    "RedisHost": "127.0.0.1",
+    "RedisPort": 6379,
+    "RedisPass": "cccccc",
+    "CacheExpired": 86400,
+    "cookieSecret": "Game.js",
+    "sessionExpiration": 86400,
+    "salt": "OdwqENkHnCse8JNt3QCCFlrnvRLxZXUP"
 }
--- socket.js
+++ socket.js
@@ -3,53 +3,51 @@ var UUID = require('node-uuid');
 var socketSession = require('./util/socket-session');
 var config = require('./config');
 
-var socket = function(server){
+var socket = function(server) {
 
-	var io = require('socket.io')(server);
+    var io = require('socket.io')(server);
 
-	io.use(function(client, next){
-		if (!client.request.headers.cookie) return next(new Error('Authentication error'));
+    io.use(function(client, next) {
+        if (!client.request.headers.cookie) return next(new Error('Authentication error'));
 
-		socketSession(client.request.headers.cookie, 'connect.sid', [config.cookieSecret], function(err, sess){
-			if(err){
-				return next(err);
-			}
-			if(sess){
-				client.user = sess.user;
-				return next();
-			}
-			else{
-				var err = new Error('UnAuthenticated');
-				err.status = 401;
-				return next(err);
-			}
-		});
-	});
+        socketSession(client.request.headers.cookie, 'connect.sid', [config.cookieSecret], function(err, sess) {
+            if (err) {
+                return next(err);
+            }
+            if (sess) {
+                client.user = sess.user;
+                return next();
+            } else {
+                var err = new Error('UnAuthenticated');
+                err.status = 401;
+                return next(err);
+            }
+        });
+    });
 
-	io.on('connection', function(client){
-	  	client.userid = UUID();
-	  	client.status = 'connected';
+    io.on('connection', function(client) {
+        client.userid = UUID();
+        client.status = 'connected';
 
-	  	client.emit('onconnected', { id: client.userid } );
-		console.log('\t socket.io:: player ' + client.userid + ' connected');
-	  	/*client.on('my other event', function (data) {
+        client.emit('onconnected', { id: client.userid });
+        console.log('\t socket.io:: player ' + client.userid + ' connected');
+        /*client.on('my other event', function (data) {
 	    	console.log(data);
 	  	});*/
-	  	
-	  	//game.findGame(client);
 
-	  	client.on('message', function(m) {
-	    	game.onMessage(client, m);
-	  	});
+        //game.findGame(client);
 
-	  	client.on('disconnect', function () {
-	    	if(client.gameid) {
-	      		console.log('\t socket.io:: client disconnected ' + client.userid + ' ' + client.gameid);
-	      		game.endGame(client);
-	    	}
-	  	});
-	});
+        client.on('message', function(m) {
+            game.onMessage(client, m);
+        });
+
+        client.on('disconnect', function() {
+            if (client.gameid) {
+                console.log('\t socket.io:: client disconnected ' + client.userid + ' ' + client.gameid);
+                game.endGame(client);
+            }
+        });
+    });
 }
-
 
 module.exports = socket;
